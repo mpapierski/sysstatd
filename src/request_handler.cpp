765a3092bc28fd0135f559840fa7d242898e8d15
Michał Papierski <michal@papierski.net>
2014-09-21 19:17:54 +0200

    Added jsoncpp and API description at "/".

--- src/request_handler.cpp
+++ src/request_handler.cpp
@@ -1,6 +1,19 @@
 #include "sysstatd/request_handler.hpp"
+#include "json/json.h"
 
 void request_handler::operator()(const connection_type::pointer & ptr)
 {
-	ptr->send_response(200, "Hello world");
+	std::cout << "URL: " << ptr->get_request_url() << std::endl;
+	if (ptr->get_request_url() == "/")
+	{
+		Json::Value resources;
+		resources["/cpu"] = "CPU stats";
+		resources["/mem"] = "MEM stats";
+		Json::Value res;
+		res["version"] = "1.0";
+		res["resources"] = resources;
+		ptr->send_response(200, res.toStyledString());
+		return;
+	}
+	ptr->send_response(404, "Unknown endpoint");
 }
